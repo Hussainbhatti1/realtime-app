a7752d572e18b0b104d7b3e41d7c5e4810aa5166
Hussain Bhatti <hussain.nuces@gmail.com>
2025-07-31 19:05:19 -0400

    update code

--- keyvault.js
+++ keyvault.js
@@ -1,15 +1,17 @@
-const { DefaultAzureCredential } = require('@azure/identity');
-const { SecretClient } = require('@azure/keyvault-secrets');
+// keyvault.js
+const { DefaultAzureCredential } = require("@azure/identity");
+const { SecretClient } = require("@azure/keyvault-secrets");
 
-const keyVaultName = process.env.KEYVAULT_NAME || "project4KeyVault321";
-const kvUri = `https://${keyVaultName}.vault.azure.net`;
+const keyVaultName = "project4KeyVault321"; // 🔁 Replace if your Key Vault name is different
+const KVUri = `https://${keyVaultName}.vault.azure.net`;
 
-const credential = new DefaultAzureCredential();
-const client = new SecretClient(kvUri, credential);
+const credential = new DefaultAzureCredential(); // uses Azure CLI credentials if logged in
+
+const client = new SecretClient(KVUri, credential);
 
 async function getDbConnectionString() {
-  const secret = await client.getSecret("DB-Connection-String");
-  return secret.value;
+  const latestSecret = await client.getSecret("DB-Connection-String");
+  return latestSecret.value;
 }
 
 module.exports = getDbConnectionString;
